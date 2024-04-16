48e83eb3009c863310ec63fdef10ade53d5e137f
zarshab <zarshabrafiq1092@gmail.com>
2024-04-16 16:48:31 +0500

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -14,13 +14,28 @@ const getData = async () => {
 export default async function Home() {
   const res = await getData();
   return (
-    <div className="grid gap-3 grid-cols-2 lg:grid-cols-4">
-      <Suspense fallback={<Loading />}>
-        {res &&
-          res?.products?.map((d, i) => {
-            return <Product item={d} key={i} />;
-          })}
-      </Suspense>
+    <div>
+      <div className="h-[300px] w-full flex justify-center items-center">
+        <div className="h-full lg:w-[70%] w-[90%] bg-gray-500 rounded-sm p-5 text-center">
+          <h1 className="text-white font-bold text-4xl">
+            Nextjs App deploy in Vercel
+          </h1>
+          <p className="py-3 text-green-500 text-sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            natus doloremque dicta odio quibusdam, inventore beatae sed aliquid
+            est fuga, nulla quis nesciunt, tenetur totam enim ratione numquam
+            voluptates blanditiis?
+          </p>
+        </div>
+      </div>
+      <div className="grid gap-3 grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<Loading />}>
+          {res &&
+            res?.products?.map((d, i) => {
+              return <Product item={d} key={i} />;
+            })}
+        </Suspense>
+      </div>
     </div>
   );
 }
